03ddc0c08c916958d86bbe45b38dbd122e9c252a
Lusain_Kim <sain9212@gmail.com>
2019-02-12 23:36:20 +0900

    GameFrameWork의 이름 변경
    GameFrameWork -> GameFramework

--- WarpApiClient/Client/GameFrameWork.h
+++ WarpApiClient/Client/GameFrameWork.h
@@ -4,12 +4,12 @@
 // 무단 전재 및 재배포 금지
 // 코드 사용 시 KoreaGameMaker@gmail.com 연락하시길 바랍니다.
 
-class WGameFrameWork
+class WGameFramework
 {
 public:
-	WGameFrameWork();
+	WGameFramework();
 
-	~WGameFrameWork();
+	~WGameFramework();
 
 
 	virtual void Reset();
