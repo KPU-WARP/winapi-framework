cce3813dfc10a2ff397db9dfcbbe9baad759e774
GameForPeople <Koreagamemaker@gmail.com>
2019-03-09 03:08:13 +0900

    라이센스 주석 수정

--- WarpApiClient/Client/Client.h
+++ WarpApiClient/Client/Client.h
@@ -1,8 +1,13 @@
+/*
+	kpu-warp-winapi-framework		Copyright ⓒ https://github.com/KPU-WARP
+
+	#0. 해당 프로젝트는 한국산업기술대 게임공학과 학술 소모임 WARP의 오픈소스 WinAPI Framework입니다.
+	#1. 관련 세부 정보는 깃헙 저장소 리드미에서 확인하실 수 있습니다.
+		- https://github.com/KPU-WARP/winapi-framework
+
+	#2. 기타 문의는, KoreaGameMaker@gmail.com으로 부탁드립니다. 감사합니다 :)
+*/
+
 #pragma once
-
-// Copyright ⓒ https://github.com/KPU-WARP
-// 무단 전재 및 재배포 금지
-// 코드 사용 시 KoreaGameMaker@gmail.com 연락하시길 바랍니다.
-
 
 #include "resource.h"
--- WarpApiClient/Client/stdafx.h
+++ WarpApiClient/Client/stdafx.h
@@ -1,12 +1,17 @@
-﻿// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
+﻿/*
+	kpu-warp-winapi-framework		Copyright ⓒ https://github.com/KPU-WARP
+
+	#0. 해당 프로젝트는 한국산업기술대 게임공학과 학술 소모임 WARP의 오픈소스 WinAPI Framework입니다.
+	#1. 관련 세부 정보는 깃헙 저장소 리드미에서 확인하실 수 있습니다.
+		- https://github.com/KPU-WARP/winapi-framework
+
+	#2. 기타 문의는, KoreaGameMaker@gmail.com으로 부탁드립니다. 감사합니다 :)
+*/
+
+// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
 // 표준 시스템 포함 파일 또는 프로젝트 특정 포함 파일이 들어 있는
 // 포함 파일입니다.
 //
-
-// Copyright ⓒ https://github.com/KPU-WARP
-// 무단 전재 및 재배포 금지
-// 코드 사용 시 KoreaGameMaker@gmail.com 연락하시길 바랍니다.
-
 
 #pragma once
 
